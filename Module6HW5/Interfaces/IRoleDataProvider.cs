94bb3cb776217dc1e7daccd381eac06ad2a82722
Aleksandr Tretiakov <sashaaleks72@gmail.com>
2022-09-09 18:51:05 +0300

    Added project files

---
+++ Module6HW5/Interfaces/IRoleDataProvider.cs
@@ -0,0 +1,10 @@
+﻿using Module6HW5.Models;
+using System.Threading.Tasks;
+
+namespace Module6HW5.Interfaces
+{
+    public interface IRoleDataProvider
+    {
+        public Task<Role> GetRoleByName(string roleName);
+    }
+}
---
+++ Module6HW5/Providers/RoleDataProvider.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Module6HW5.DB;
+using Module6HW5.Interfaces;
+using Module6HW5.Models;
+using System.Threading.Tasks;
+
+namespace Module6HW5.Providers
+{
+    public class RoleDataProvider : IRoleDataProvider
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public RoleDataProvider(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Role> GetRoleByName(string roleName)
+        {
+            var role = await _dbContext.Roles.FirstOrDefaultAsync(r => r.Name == roleName);
+
+            return role;
+        }
+    }
+}
